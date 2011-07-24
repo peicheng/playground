c8c1006069c73c4808129dc464f1d5618983890d
amazingjxq <amazingjxq@gmail.com>
2011-07-24 22:15:24 +0800

    QOTD.PY

---
+++ twisted/QOTD.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+from twisted.internet.protocol import Protocol, Factory
+from twisted.internet.endpoints import *
+
+class QOTD(Protocol):
+
+    def connectionMade(self):
+        self.transport.write("An apple a day keeps the doctor away\r\n")
+        self.transport.loseConnection()
+
+factory = Factory()
+factory.protocol = QOTD
+
+endpoint = TCP4ServerEndpoint(reactor, 8007)
+endpoint.listen(factory)
+reactor.run()
