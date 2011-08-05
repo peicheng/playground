4b2d39342c8ac500dcb6ce8a7a02ea23c5303e61
amazingjxq <amazingjxq@gmail.com>
2011-08-05 18:12:26 +0800

    redirect user to their orignal url

--- shortlink/wsgi.py
+++ shortlink/wsgi.py
@@ -2,6 +2,7 @@ import web
 import random
 import string
 import redis
+import urlparse
 
 render = web.template.render('templates/')
 urls = (
@@ -11,28 +12,34 @@ urls = (
 
 app = web.application(urls, globals())
 
-r= redis.Redis(host='d6d77854.dotcloud.com', port=8603, password="6Mb3Y1IzxFaqmjM0CB4k", db=0)
+#r= redis.Redis(host='d6d77854.dotcloud.com', port=8603, password="6Mb3Y1IzxFaqmjM0CB4k", db=0)
+r= redis.Redis(host='localhost', port=6379, db=0)
 
 class index:
     def GET(self):
-        return render.index(None, None)
+        return render.index(url=None, shortcode=None)
     
     def POST(self):
         i = web.input()
         url = i.url
-        shortcode = r.get("shortcode:%s" % url)
-        if shortcode == None:
-            while True:
-                shortcode = ''.join(random.choice(string.letters + string.digits) for x in range(6))
-                if r.get("url:%s" % shortcode) == None:
-                    r.set("url:%s" % shortcode, url)
-                    break
-            r.set("shortcode:%s" % url, shortcode)
-        return render.index(url=url, shortcode=shortcode)
+        if urlparse.urlparse(url)[0] == '' and urlparse.urlparse(url)[1] == '':
+            return render.index(url=None, shortcode=None, err="error")
+        else:
+            shortcode = r.get("shortcode:%s" % url)
+            if shortcode == None:
+                while True:
+                    shortcode = ''.join(random.choice(string.letters + string.digits) for x in range(6))
+                    if r.get("url:%s" % shortcode) == None:
+                        r.set("url:%s" % shortcode, url)
+                        break
+                    r.set("shortcode:%s" % url, shortcode)
+            return render.index(url=url, shortcode=shortcode)
 
 class redirect:
     def GET(self, shortcode):
-        return render.index(url=None, shortcode=shortcode)
+        url = r.get("url:%s" % shortcode)
+        if url != None:
+            raise web.seeother(url)
 
 application = app.wsgifunc()
 
