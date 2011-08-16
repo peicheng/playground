1f52ff0f0ecb63ef6e3bab36e9d872f93a7e98f0
amazingjxq <amazingjxq@gmail.com>
2011-08-16 19:42:21 +0800

    coutDown

--- python/countDown.py
+++ python/countDown.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
-class countDown:
-    pass
+class countDown(object):
+    def __init__(self, start):
+        self.count = start
+    def __iter__(self):
+        return self
+    def next(self):
+        if self.count <= 10:
+            raise StopIteration
+        r = self.count
+        self.count -= 1
+        return r
 
 if __name__ == '__main__':
     print 'hello world'
