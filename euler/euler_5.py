f52715d660354d701afa681136e18d0b5d64f82d
amazingjxq <amazingjxq@gmail.com>
2011-08-25 18:11:11 +0800

    reduce

--- euler/euler_5.py
+++ euler/euler_5.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
 def gcd(a, b):
-    print a, b
     if b == 0:
         return a
     return gcd(b, a % b)
 
 def gcd_m(l):
-    reduce(gcd, l)
+    return reduce(gcd, l)
 
 if __name__ == '__main__':
     print gcd_m(range(1, 10))
