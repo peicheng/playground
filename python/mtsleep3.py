b80cd1c6d492ace5a29c356fbd3bc55feff332e8
amazingjxq <amazingjxq@gmail.com>
2011-09-27 13:44:10 +0800

    mtsleep3.py

--- python/mtsleep3.py
+++ python/mtsleep3.py
@@ -12,7 +12,20 @@ def loop(nloop, nsec):
 
 
 def main():
-    pass
+    print "starting at:", ctime()
+    threads = []
+    nloops = range(len(loops))
+    for i in nloops:
+        t = threading.Thread(target=loop, args=(i, loops[i]))
+        threads.append(t)
+
+    for i in nloops:
+        threads[i].start()
+
+    for i in nloops:
+        threads[i].join()
+
+    print 'all DONE at:', ctime()
 
 
 if __name__ == '__main__':
