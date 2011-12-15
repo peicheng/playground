a681723eea7c3e6a403f3e078566b4016791ec27
amazingjxq <amazingjxq@gmail.com>
2011-12-15 16:53:44 +0800

    BookStore.hs

--- haskell/BookStore.hs
+++ haskell/BookStore.hs
@@ -1,2 +1,10 @@
 data BookInfo = Book Int String [String]
                 deriving (Show)
+
+myInfo = Book 9780135072455 "Algebra of Programming"
+         ["Richard Bird", "Oege de Moor"]
+         
+type CustomerID = Int
+type ReviewBody = String
+
+data BetterReview = BetterReview BookInfo CustomerID ReviewBody
